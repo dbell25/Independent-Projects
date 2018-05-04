d5d51d916c0afda1c811af8c0d498c46ebb5752d
Daniel <danieljbell@ksu.edu>
2018-05-04 11:12:44 -0500

    MMS - Classes started

---
+++ Multi Matrix Solver/Matrix MultiTool/MatrixSolver/Model/HistoryDB.cs
@@ -0,0 +1,14 @@
+﻿using MatrixSolver.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixSolver.Model
+{
+    public static class HistoryDB
+    {
+        private static List<History> history;
+    }
+}
